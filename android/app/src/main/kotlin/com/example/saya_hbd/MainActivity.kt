127929dfc0423622ba2b033f23f064d7dae120c7
komaz0130 <t.komaz0130@icloud.com>
2020-06-16 16:23:02 +0900

    初回コミット

---
+++ android/app/src/main/kotlin/com/example/saya_hbd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.saya_hbd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'What Day is it Today'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_counter == 21){
+      return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'How old are you:',
+            ),
+            Text(
+              'Happy ${_counter}st Birthday!!!',
+              style: TextStyle(
+              fontSize:65,
+              fontWeight:FontWeight.bold,
+              color:Colors.redAccent,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.red,
+              ),
+            ),
+            Text("Dear Sayaka,"),
+            Text("You are the kindest person I have ever known."),
+            Text("So you are such a big inspiration to me."),
+            Text("I am so blessed that you are in my life."),
+            Text("I hope you have a special day!"),
+            Text("I love you 3000..."),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+    }else{
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'How old are you:',
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(
+              fontSize:72,
+              fontWeight:FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+  }}
